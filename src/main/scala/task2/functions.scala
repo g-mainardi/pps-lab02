75a30636d44ac87dfdeea7da240d9cbe7ec012b9
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-03-15 11:58:49 +0100

    Tasks - part 4 9) Copied code in functions, general lab file

--- src/main/scala/task2/functions.scala
+++ src/main/scala/task2/functions.scala
@@ -218,3 +218,20 @@ object functions extends App:
   testEquals(NUMBER1_REVERSE, reverseNumber(NUMBER1))
   testEquals(NUMBER2_REVERSE, reverseNumber(NUMBER2))
   testEquals(NUMBER3_REVERSE, reverseNumber(NUMBER3))
+
+  printSection("9 (Test in dedicated package) ")
+  enum Expr:
+    case Literal(constant: Int)
+    case Add(subExpr1: Expr, subExpr2: Expr)
+    case Multiply(subExpr1: Expr, subExpr2: Expr)
+
+  object Expr:
+    def evaluate(expr: Expr): Int = expr match
+      case Literal(c) => c
+      case Add(e1, e2) => evaluate(e1) + evaluate(e2)
+      case Multiply(e1, e2) => evaluate(e1) * evaluate(e2)
+
+    def show(expr: Expr): String = expr match
+      case Literal(c) => "" + c
+      case Add(e1, e2) => "(" + show(e1) + " + " + show(e2) + ")"
+      case Multiply(e1, e2) => "(" + show(e1) + " * " + show(e2) + ")"
